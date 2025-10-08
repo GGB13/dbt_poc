3a003a726db561a0c377b5d4daf990d9b0c171ea
GGB13 <57085171+GGB13@users.noreply.github.com>
2025-10-09 00:43:34 +0530

    Delete macros/models/marts/intermediate directory

--- macros/models/marts/intermediate/user_activity.sql
+++
@@ -1,7 +0,0 @@
-select
-  user_id,
-  date_trunc('day', event_time) as activity_date,
-  count(*) as events_count,
-  count(distinct event_type_clean) as unique_event_types
-from {{ ref('stg_events') }}
-group by user_id, activity_date
